75ddd7462fc06ceba4653b5f201ade10fb772e45
hgepryan <hgepryan@18c9370e-b239-f774-4d76-eeb75c8aec0f>
2012-05-28 06:41:24 +0000

    MVC3 en .net Razor

---
+++ Fuentes/Aplicativo/TrabajoDSDRazor/DSDESBMVCRazor/Models/Especialidad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DSDESBMVCRazor.Models
+{
+    public class Especialidad
+    {
+        [Required]
+        [Display(Name = "Codigo")]
+        public string Codigo { get; set; }
+
+
+        [Required]
+        [Display(Name = "Descripcion")]
+        public string Descripcion { get; set; }
+
+        public char Estado { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string UsuarioRegistro { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public string UsuarioModificacion { get; set; }
+    }
+}
---
+++ Fuentes/Aplicativo/TrabajoDSDRazor/DSDESBMVCRazor/Models/PersonalMedico.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace DSDESBMVCRazor.Models
+{
+    public class PersonalMedico
+    {
+        public string Codigo { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMAterno { get; set; }
+        public string Nombres { get; set; }
+        public char TIPODOCUMENTOID { get; set; }
+        public string NumeroDocumentoID { get; set; }
+        public string EspecialidadID { get; set; }
+        public string NumeroCMP { get; set; }
+        public string FechaNacimiento { get; set; }
+        public char EstadoCivil { get; set; }
+        public string Direccion { get; set; }
+        public string TelefonoFijo { get; set; }
+        public string MovilMovistar { get; set; }
+        public string RPM { get; set; }
+        public string MovilClaro { get; set; }
+        public string RPC { get; set; }
+        public char Estado { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string UsuarioRegistro { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public string UsuarioModificacion { get; set; }
+    }
+}
