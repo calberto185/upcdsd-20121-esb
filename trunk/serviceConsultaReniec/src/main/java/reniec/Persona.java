c00822ebef4e87549b377da4e80e6765a9854fb3
felixangelalfaroc@gmail.com <felixangelalfaroc@gmail.com@18c9370e-b239-f774-4d76-eeb75c8aec0f>
2012-05-29 06:56:20 +0000

    servicio SOAP que emula una respuesta de RENIEC.... Versión estable usando contract firts.

--- trunk/serviceConsultaReniec/src/main/java/reniec/Persona.java
+++ trunk/serviceConsultaReniec/src/main/java/reniec/Persona.java
@@ -2,33 +2,78 @@ package reniec;
 
 public class Persona {
 	
+	private String dni;
 	private String nombre;
-	private String apellido;
-	private String ciudad;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String fechaNacimiento;
+	private String direccion;
+	private String estadoCivil;
+	
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getApellido() {
-		return apellido;
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
 	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
 	}
-	public String getCiudad() {
-		return ciudad;
+
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
 	}
-	public void setCiudad(String ciudad) {
-		this.ciudad = ciudad;
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
 	}
-	
-	public Persona(String nomb, String ape, String ciu) {
-		this.nombre=nomb;
-		this.apellido=ape;
-		this.ciudad=ciu;
-		
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public Persona(String dniTmp, String nombreTmp, String apePat, String apeMat, String fecNac, String direc, String estado) {
+		this.dni=dniTmp;
+		this.nombre= nombreTmp;
+		this.apellidoPaterno=apePat;
+		this.apellidoMaterno=apeMat;
+		this.fechaNacimiento=fecNac;
+		this.direccion=direc;
+		this.estadoCivil=estado;
 	}
 	
 }
--- trunk/serviceConsultaReniec/src/main/java/reniec/internal/ReniecConsultaImpl.java
+++ trunk/serviceConsultaReniec/src/main/java/reniec/internal/ReniecConsultaImpl.java
@@ -1,19 +1,48 @@
 package reniec.internal;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import reniec.Persona;
-import reniec.ReniecConsulta;;
+import reniec.ReniecConsulta;
 
 @Service("reniecConsulta")
 public class ReniecConsultaImpl implements ReniecConsulta {
+	
+	@Autowired
+	public ReniecConsultaImpl() {
+		
+	}
 
 	@Transactional
 	public Persona consultaDatos(String dni) {
+		
+		List<Persona> lstPersona =new ArrayList<Persona>();
+		Persona persona=null;
 
-		Persona person = new Persona("Juan","Camaney","Tangamandapio");
-		return person;
+		persona = new Persona("41645302", "Felix", "Alfaro", "cayllahua", "26/01/2012", "Av Micaela", "S");
+		lstPersona.add(persona);
+		persona = new Persona("41645303", "Felix1", "ApePat1", "ApeMat1", "27/01/2012", "Av Micaela", "S");
+		lstPersona.add(persona);
+		persona = new Persona("41645304", "Felix2", "ApePat2", "ApeMat2", "28/01/2012", "Av Micaela", "S");
+		lstPersona.add(persona);
+		persona = new Persona("41645305", "Felix3", "ApePat3", "ApeMat3", "29/01/2012", "Av Micaela", "S");
+		lstPersona.add(persona);
+		persona = new Persona("41645306", "Felix4", "ApePat4", "ApeMat4", "30/01/2012", "Av Micaela", "S");
+		lstPersona.add(persona);
+		
+		persona=null;
+		for(int i=0; i<= lstPersona.size();i++){
+			if (lstPersona.get(i).getDni().equals(dni)){
+				persona=lstPersona.get(i);
+				break;
+			} 
+		}
+		return persona;
 	}
 
 }
