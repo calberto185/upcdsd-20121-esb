fd51de67a2127cce1c7c908f8d1cdd0bdb0feae8
felixangelalfaroc <felixangelalfaroc@gmail.com>
2012-06-15 22:45:17 +0000

    actualización de usuarios en memoria.

---
+++ servicioSBSValidate/src/main/java/pe/sbs/domain/Asegurado.java
@@ -0,0 +1,20 @@
+package pe.sbs.domain;
+
+public class Asegurado {
+	
+	private String dni;
+	private String aseguradora;
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getAseguradora() {
+		return aseguradora;
+	}
+	public void setAseguradora(String aseguradora) {
+		this.aseguradora = aseguradora;
+	}
+	
+}
