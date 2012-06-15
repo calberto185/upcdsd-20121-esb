a5385b905be0611441070192750b8cacb8da3c2a
felixangelalfaroc@gmail.com <felixangelalfaroc@gmail.com@18c9370e-b239-f774-4d76-eeb75c8aec0f>
2012-06-15 22:45:17 +0000

    actualización de usuarios en memoria.

--- trunk/servicioSBSValidate/src/main/java/pe/sbs/ValidaImpl.java
+++ trunk/servicioSBSValidate/src/main/java/pe/sbs/ValidaImpl.java
@@ -1,6 +1,11 @@
 package pe.sbs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebService;
+
+import pe.sbs.domain.Asegurado;
 
 /**
  * 
@@ -9,8 +14,38 @@ import javax.jws.WebService;
 
 @WebService(endpointInterface = "pe.sbs.Valida")
 public class ValidaImpl implements Valida {
+	
+	private List<Asegurado> lstDocumentos;
+	
+	public ValidaImpl(){
+		lstDocumentos = new ArrayList<Asegurado>();
+		
+		Asegurado asegurado = new Asegurado();
+		asegurado.setDni("41645302");
+		asegurado.setAseguradora("RIMAC");
+		lstDocumentos.add(asegurado);
+		
+		asegurado = new Asegurado();
+		asegurado.setDni("41645303");
+		asegurado.setAseguradora("PACIFICO");
+		lstDocumentos.add(asegurado);
+		
+		asegurado = new Asegurado();
+		asegurado.setDni("41645302");
+		asegurado.setAseguradora("MAPFRE");
+		lstDocumentos.add(asegurado);
+	}
 
 	public String validarSeguroUsuario(String dni) {
-		return "RIMAC";
+		String EmpresaAseguradora="NINGUNO";
+		for(Asegurado asegurado: lstDocumentos){
+			if(dni.equals(asegurado.getDni())){
+				EmpresaAseguradora = asegurado.getAseguradora(); 
+				break;
+			}
+		}
+		
+		return EmpresaAseguradora;
+		
 	}
 }
