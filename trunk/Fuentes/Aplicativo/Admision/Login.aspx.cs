963e9aa51fe2f5263364dd07126dee3cb386f4e6
felixangelalfaroc@gmail.com <felixangelalfaroc@gmail.com@18c9370e-b239-f774-4d76-eeb75c8aec0f>
2012-06-15 22:34:12 +0000

    Integración App Web Net con los setrvicios SOAP

--- trunk/Fuentes/Aplicativo/Admision/Login.aspx.cs
+++ trunk/Fuentes/Aplicativo/Admision/Login.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using Admision.Models;
 
 namespace Admision
 {
@@ -21,7 +23,51 @@ namespace Admision
 
         protected void LoginButton_Click(object sender, EventArgs e)
         {
+            //--------------------------INI----------------------
+            //Invocación Servicio Web SOAP Contrat Firts - Reniec
+            //---------------------------------------------------
+
+            WSReniec.ReniecService servicio = new WSReniec.ReniecService();
+
+            WSReniec.dniRequest dniRequest = new WSReniec.dniRequest();
+            Admision.WSReniec.DniType dniType = new Admision.WSReniec.DniType();
+            dniType.dni = "41645302";//Reemplazar por valor en caja de texto
+
+            dniRequest.dni = dniType;
+
+            String strTramaXML = servicio.dni(dniRequest);
+
+            XmlDocument xmlDocumento = new XmlDocument();
+            XmlNodeList xmlListaNodoTrama;
+            xmlDocumento.LoadXml(strTramaXML);
+
+            xmlListaNodoTrama = xmlDocumento.GetElementsByTagName("Root");
+
+            Persona persona = new Persona();
+            XmlNode nodo = xmlListaNodoTrama.Item(0);
+            XmlElement xmlSubElement = nodo["Root"];
+
+            persona.Nombre = xmlListaNodoTrama[0]["nombre"].InnerText;
+            persona.ApellidoPaterno = xmlListaNodoTrama[0]["apellidoPaterno"].InnerText;
+            persona.ApellidoMaterno = xmlListaNodoTrama[0]["apellidoMaterno"].InnerText;
+            persona.Dni = xmlListaNodoTrama[0]["dni"].InnerText;
+            persona.Direccion = xmlListaNodoTrama[0]["direccion"].InnerText;
+            persona.FechaNacimiento = xmlListaNodoTrama[0]["fechaNacimiento"].InnerText;
+            persona.EstadoCivil = xmlListaNodoTrama[0]["estadoCivil"].InnerText;
+            //--------------------------FIN----------------------
+
+
+            //--------------------------INI----------------------
+            //Invocación Servicio Web SOAP Contrat Last - SBS
+            //---------------------------------------------------
+            WSSBS.ValidaImplService servicioSBS = new WSSBS.ValidaImplService();
+            String empresaAseguradora = servicioSBS.validarSeguroUsuario("41645302");//Reemplazar por valor en caja de texto
+            //--------------------------FIN----------------------
+                       
+            
             Response.Redirect("/");
+
+
         }
     }
 }
